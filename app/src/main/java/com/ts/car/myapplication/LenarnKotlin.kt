1c2ac701662526e91d98f951a890181b4c502fb5
wei.liu <wei.liu@thundersoft.com>
2022-06-15 09:51:57 +0800

    Kotlin Learn

--- app/src/main/java/com/ts/car/myapplication/LenarnKotlin.kt
+++ app/src/main/java/com/ts/car/myapplication/LenarnKotlin.kt
@@ -52,6 +52,36 @@ fun largerNumber5(num1: Int, num2: Int) : Int {
 fun  largerNumber6(num1: Int, num2: Int) = if (num1 > num2) num1  else num2
 
 
+fun getSore(name : String) = if (name == "Tom") {
+    86
+} else if (name == "Jim") {
+    95
+} else {
+    100
+}
+
+fun getSore2(name : String) = when (name) {
+    "Tom" -> 86
+    "jam" -> 77
+    "Lily" -> 100
+    else -> 0
+}
+
+// is = instanceof
+fun checkNumber(num : Number) {
+    when (num) {
+        is Int -> println("number is Int")
+        is Double -> println("number is Double")
+        else -> println("number not support")
+    }
+}
+
+// Kotlin中判断字
+//符串或对象是否相等可以直接使用==关键字, 而不用像Java那样调用equals()
+fun
+
+
+
 class LenarnKotlin {
 
 }
